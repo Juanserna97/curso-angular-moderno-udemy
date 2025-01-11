a670ac4db0a782c03425457130dec74a770372f9
Juan Serna <juanserna427@gmail.com>
2025-01-11 00:59:05 -0500

    migrate observable to signals

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,11 +1,8 @@
 import { Injectable, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 export class SpinnerService {
-  private readonly isLoading = signal<boolean>(false);
-
-  isLoading$ = toObservable(this.isLoading);
+  readonly isLoading = signal<boolean>(false);
 
   show(): void {
     this.isLoading.set(true);
