b2a2595aea392a2e136149c798034276b3bc4ad2
Juan Serna <juanserna427@gmail.com>
2025-01-11 00:56:05 -0500

    migrate spinner to signals

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 export class SpinnerService {
-  private isLoadingSubject = new BehaviorSubject<boolean>(false);
-  isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
+  private readonly isLoading = signal<boolean>(false);
+
+  isLoading$ = toObservable(this.isLoading);
 
   show(): void {
-    this.isLoadingSubject.next(true);
+    this.isLoading.set(true);
   }
 
   hide(): void {
-    this.isLoadingSubject.next(false);
+    this.isLoading.set(false);
   }
 }
